d8e10e2ced31ef977df23d2155da35303d538745
carlos.b.feitoza.filho <carlos.b.feitoza.filho@4d8486d2-a66b-2f1f-367a-5b6457fbdc6a>
2010-12-27 15:48:35 +0000

    - INCLUÍDA VERIFICAÇÃO DE LICENÇAS BASEADAS EM SERIAL DE HD - INCLUÍDO UM PEQUENO APLICATIVO PARA CONVERSÃO DE SERIAIS EM UM HASH - INCLUÍDA A DLL DE VERIFICAÇÃO DE HD - INCLUÍDO UM ARQUIVO DE LICENÇA DE EXEMPLO - INCLUÍDA UMA UNIT PARA OBTENÇÃO DE INFORMAÇÕES DO HD - ALTERADA A UNIT "UFUNCOES.PAS"

--- WELL PLAYER/PRJ/WellPlayer.dpr
+++ WELL PLAYER/PRJ/WellPlayer.dpr
@@ -1,17 +1,45 @@
 program WellPlayer;
 
 uses
-  Forms,
+  Forms, SysUtils, Windows,
   UForm_Configuracao in '..\src\UForm_Configuracao.pas' {Form_Configuracao},
   UFuncoes in '..\src\UFuncoes.pas',
-  UForm_Player in '..\src\UForm_Player.pas' {Form_Player};
+  UForm_Player in '..\src\UForm_Player.pas' {Form_Player},
+  UHDDInfo in '..\SRC\UHDDInfo.pas',
+  ZTO.Crypt.Utilities in '..\SRC\Crypt\ZTO.Crypt.Utilities.pas',
+  ZTO.Crypt.Types in '..\SRC\Crypt\ZTO.Crypt.Types.pas',
+  ZTO.Crypt.Hashes.Haval in '..\SRC\Crypt\hashes\ZTO.Crypt.Hashes.Haval.pas',
+  ZTO.Crypt.Hashes.Md4 in '..\SRC\Crypt\hashes\ZTO.Crypt.Hashes.Md4.pas',
+  ZTO.Crypt.Hashes.Md5 in '..\SRC\Crypt\hashes\ZTO.Crypt.Hashes.Md5.pas',
+  ZTO.Crypt.Hashes.Ripemd128 in '..\SRC\Crypt\hashes\ZTO.Crypt.Hashes.Ripemd128.pas',
+  ZTO.Crypt.Hashes.Ripemd160 in '..\SRC\Crypt\hashes\ZTO.Crypt.Hashes.Ripemd160.pas',
+  ZTO.Crypt.Hashes.Sha1 in '..\SRC\Crypt\hashes\ZTO.Crypt.Hashes.Sha1.pas',
+  ZTO.Crypt.Hashes.Sha256 in '..\SRC\Crypt\hashes\ZTO.Crypt.Hashes.Sha256.pas',
+  ZTO.Crypt.Hashes.Sha512 in '..\SRC\Crypt\hashes\ZTO.Crypt.Hashes.Sha512.pas',
+  ZTO.Crypt.Hashes.tiger in '..\SRC\Crypt\hashes\ZTO.Crypt.Hashes.tiger.pas',
+  ZTO.Crypt.Common in '..\SRC\Crypt\ZTO.Crypt.Common.pas',
+  ZTO.Crypt.Consts in '..\SRC\Crypt\ZTO.Crypt.Consts.pas',
+  ZTO.Crypt.Base64 in '..\SRC\Crypt\ZTO.Crypt.Base64.pas',
+  ZTO.Crypt.Blockciphers in '..\SRC\Crypt\ZTO.Crypt.Blockciphers.pas';
 
 {$R *.res}
 
 begin
-  Application.Initialize;
-  Application.Title := 'Well Player';
-  Application.CreateForm(TForm_Configuracao, Form_Configuracao);
-  Application.Run;
+
+
+
+  try
+    Application.Initialize;
+    VerificarLicenca;
+    Application.Title := 'Well Player';
+    Application.CreateForm(TForm_Configuracao, Form_Configuracao);
+    Application.Run;
+  except
+    on E: Exception do
+    begin
+      Application.MessageBox(PChar(E.Message),'Execução não autorizada',MB_ICONWARNING);
+      Application.Terminate;
+    end;
+  end;
 end.
 
---
+++ WELL PLAYER/SRC/LIC GENERATOR/UPrincipal.pas
@@ -0,0 +1,35 @@
+unit UPrincipal;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs, StdCtrls, ExtCtrls;
+
+type
+  TForm_Principal = class(TForm)
+    LabeledEdit_Origem: TLabeledEdit;
+    LabeledEdit_Destino: TLabeledEdit;
+    Button_Gerar: TButton;
+    procedure Button_GerarClick(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form_Principal: TForm_Principal;
+
+implementation
+
+{$R *.dfm}
+
+uses ZTO.Crypt.Utilities, ZTO.Crypt.Types;
+
+procedure TForm_Principal.Button_GerarClick(Sender: TObject);
+begin
+  LabeledEdit_Destino.Text := GetStringCheckSum(LabeledEdit_Origem.Text,[haSha512]);
+end;
+
+end.
