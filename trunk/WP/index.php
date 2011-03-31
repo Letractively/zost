faca276ce03145a1a93c8110c3d439ca0ebe1c18
carlos.b.feitoza.filho@gmail.com <carlos.b.feitoza.filho@gmail.com@4d8486d2-a66b-2f1f-367a-5b6457fbdc6a>
2011-03-31 17:19:28 +0000

    Estrutura inicial do site

---
+++ trunk/WP/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="pt" lang="pt">
+  <head>
+    <title>:: WOLF PAINTBALL ::</title>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <link rel="stylesheet" type="text/css" href="css/estilos.css" />
+    <script type="text/javascript" src="jsc/frw/ptt/prototype.js"></script>
+    <script type="text/javascript" src="jsc/frw/sct/scriptaculous.js"></script>
+    <script type="text/javascript">
+    //<![CDATA[
+    //]]>
+    </script>
+  </head>
+  <body>
+    <div class="site">
+      <div class="cabecalho"></div>
+      <div class="menu"></div>
+      <div class="propagandas"></div>
+      <div class="conteudo">
+<? require("./php/conteudo.php"); ?>
+      </div>
+      <div class="rodape"></div>
+    </div>
+  </body>
+</html>
